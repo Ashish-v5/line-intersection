1351290c717efc3b3091f9593ba4c9e40af8b67f
Daniil <Git@daniil.best>
2021-06-29 12:53:58 +0300

    initial commit

--- src/test/java/ru/danka/intersection/LineIntersectionTest.java
+++ src/test/java/ru/danka/intersection/LineIntersectionTest.java
@@ -1,24 +1,21 @@
 package ru.danka.intersection;
 
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.concurrent.ThreadLocalRandom;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class LineIntersectionTest {
     @Test
     public void resultTest(){
         Line line = new Line(1,2);
         Line line2 = new Line(2,1);
-        Assert.assertEquals(1, line.getIntersection(line2).getX());
+        Assertions.assertEquals(1, line.getIntersection(line2).getX());
     }
 
     @Test
     public void divideByZeroTest(){
         Line line = new Line(0,0);
         Line line2 = new Line(0,0);
-        Assert.assertEquals(null, line.getIntersection(line2));
+        Assertions.assertEquals(null, line.getIntersection(line2));
     }
 }
